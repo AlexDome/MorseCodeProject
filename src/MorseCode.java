4134a10083a3e8af01a558465ae6afd1ac00b4b2
Alex Dome <alex.dome.2022@student.lhschools.org>
2019-05-22 14:10:28 -0400

    Flipped arrays

--- src/MorseCode.java
+++ src/MorseCode.java
@@ -1,2 +1,14 @@
+
+import java.util.Scanner;
+
 public class MorseCode {
+
+    public static void main(String[] args){
+
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+
+
+        System.out.println();
+    }
 }
